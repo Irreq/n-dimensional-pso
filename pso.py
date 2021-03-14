6ae3134914f453c32796bbd3a7f35bcf6e04afe2
Irreq <irreq@protonmail.com>
2021-03-14 13:03:59 +0100

    upated file

--- pso.py
+++ pso.py
@@ -11,8 +11,8 @@
 
 seed = 2.718281828459045235360287471352662497757
 
-def random(multiplier=69069, increment=1, modulus=2**16):
 
+def random(multiplier=69069, increment=1, modulus=2**16):
     """
     Linear Congruential Generator (LCG)
 
@@ -33,11 +33,11 @@ def random(multiplier=69069, increment=1, modulus=2**16):
                             the behavior of the particles.
 
     ARGUMENTS:
-        - multiplier        int() 0 < a < m
+        - multiplier        int() 0 < 'a' < 'm'
 
-        - increment         int() 0 <= c < m
+        - increment         int() 0 <= 'c' < 'm'
 
-        - modulus           int() 0 < m
+        - modulus           int() 0 < 'm'
 
     """
 
@@ -48,8 +48,8 @@ def random(multiplier=69069, increment=1, modulus=2**16):
 
     return seed / modulus
 
-def uniform(low, high):
 
+def uniform(low, high):
     """
     Generate a random value between low and high
 
@@ -61,8 +61,8 @@ def uniform(low, high):
 
     return abs(high-low) * random() + low
 
-def ndistance(p1, p2):
 
+def ndistance(p1, p2):
     """
     Calculate eucleidian distance between
     two points in N-dimensional space
@@ -70,48 +70,77 @@ def ndistance(p1, p2):
 
     return sum([(p1[i] - p2[i])**2 for i in range(len(p1))])**0.5
 
-def optimize(function, *, domain: list(), target: "see documentation", n=25, dims=False, iters=100, convergence=0.001, vmax=0.1, personal=2.0, social=2.0) -> random():
 
+def check():
+    return
+
+
+def optimize(function, *, domain: list(), target: "see documentation",
+             n=25, dims=False, iters=100, convergence=0.001, vmax=0.1,
+             personal=2.0, social=2.0) -> random():
     """
-    Omni Dimensional Particle Swarm Optimization without external libraries (RAW Python)
+    Omni Dimensional Particle Swarm Optimization
+    without external libraries (RAW Python)
 
-    DOCUMENTATION:          Read this or see: 'README.md' or read comments in functions
+    DOCUMENTATION:          Read this or see: 'README.md' or read comments in
+                            functions
 
     ARGUMENTS:
-        - function          function() The function to optimize without calling it just send the
-                            variable name. Eg, 'ackley'
+        - function          function() The function to optimize without calling
+                            it just send the variable name. Eg, 'ackley'
 
     KEYWORD ARGUMENTS:
-        - target            What to look for. The program will try to find a certain way to get as close
-                            to the specified keyword argument as possible. If you wan't to find which varibles
-                            leads to a result closest to eg, 0.6. Then specify target as 'target=0.6'. If you want
-                            to find the extrema of a function you can call either 'maximum' or 'minimum'.
+        - target            What to look for. The program will try to find a
+                            certain way to get as close to the specified
+                            keyword argument as possible. If you wan't to find
+                            which varibles leads to a result closest to eg,
+                            0.6. Then specify target as 'target=0.6'. If you
+                            want to find the extrema of a function you can call
+                            either 'maximum' or 'minimum'.
 
-        - domain            list() The search space of the function 'function' to be optimized. N-directional search
-                            space for non cartesian environments. If nothing else presented, the system thinks first
-                            position is lower boundary, and the second, the upper boundary. Eg, [-2, 4]
+        - domain            list() The search space of the function 'function'
+                            to be optimized. N-directional search space for non
+                            cartesian environments. If nothing else presented,
+                            the system thinks first position is lower boundary,
+                            and the second, the upper boundary. Eg, [-2, 4]
 
-        - n                 int() How many particles/n_particles to utilize. Eg, 25
+        - n                 int() How many particles/n_particles to utilize.
+                            Eg, 25
 
-        - dims              int() Number of dimensions in the function to optimize. If left as 'None', the program will
-                            look up the number of positional arguments in the function to optimize; 'function' as following:
+        - dims              int() Number of dimensions in the function to
+                            optimize. If left as 'None', the program will
+                            look up the number of positional arguments in the
+                            function to optimize; 'function' as following:
 
+                            ```
                             def function(a, b, c, *args, **kwargs):
                                 ...
                                 return int()
+                            ```
 
-                            In this case, the program will think the function has 3 dimensions, as of the three positional arguments
-                            If dims is not 'None', the program will populate the function as specified by the user. Eg, 4
+                            In this case, the program will think the function
+                            has 3 dimensions, as of the three positional
+                            arguments. If 'dims' is not 'None', the program
+                            will populate the function as specified by the
+                            user. Eg, 4
 
         - iters             int() Maximum number of iterrations. Eg, 100
 
         - convergence       float() Convergence value. Eg, 0.001
 
-        - vmax              float() Maximum velocity value for particle. Eg, 0.1
+        - vmax              float() Maximum velocity value for particle.
+                            Eg, 0.1
 
-        - personal          float() Particle personal coefficient factor. Eg, 2.0
+        - personal          float() Particle personal coefficient factor.
+                            Eg, 2.0
 
-        - social            float() Particle social coefficient factor. Eg, 2.0
+        - social            float() Particle social coefficient factor.
+                            Eg, 2.0
+
+    RETURNS:
+        - coordinates       list() Global best coordinates. Eg, [0.31, 4.21]
+
+        - best              float() The solution. Eg, -3.21
 
     TODO:                   Finnish documentation
                             + the dancers and killers.
@@ -120,7 +149,10 @@ def optimize(function, *, domain: list(), target: "see documentation", n=25, dim
 
     if not dims:
         # Neat way to find number of positional arguments in a function
-        dims = function.__code__.co_argcount - len(function.__defaults__) if function.__defaults__ != None else function.__code__.co_argcount
+        if function.__defaults__ is not None:
+            dims = function.__code__.co_argcount - len(function.__defaults__)
+        else:
+            dims = function.__code__.co_argcount
 
     # Populate the swarm
     swarm = []
@@ -128,17 +160,16 @@ def optimize(function, *, domain: list(), target: "see documentation", n=25, dim
     for id in range(n):
 
         # Initialize velocity for particle
-        velocity = [random()*vmax,]*dims
+        velocity = [random()*vmax, ]*dims
 
         # Generate initial coordinates within the 'domain'
-        coordinates = [uniform(*domain),]*dims
+        coordinates = [uniform(*domain), ]*dims
 
         # Initial value from the particle's coordinates
         extreme = function(*coordinates)
 
         # Particle structure: [id, velocity, extreme, coordinates, best]
         swarm.append([id, velocity, extreme, coordinates, coordinates.copy()])
-
 
     # What would later be the best particle, but for now just the last one
     coordinates = swarm[-1][3].copy()
@@ -174,7 +205,8 @@ def optimize(function, *, domain: list(), target: "see documentation", n=25, dim
                 swarm[particle][3][i] += swarm[particle][1][i]
 
                 # Update swarm[particle] position to fit boundaries
-                if not domain[0] < swarm[particle][3][i] < domain[1]: # FIX THIS FOR MULTI DOMAINS
+                # FIX THIS FOR MULTI DOMAINS
+                if not domain[0] < swarm[particle][3][i] < domain[1]:
                     swarm[particle][3][i] = uniform(domain[0], domain[1])
 
             # Optimize for solution to 'function'
@@ -205,13 +237,15 @@ def optimize(function, *, domain: list(), target: "see documentation", n=25, dim
                 if abs(swarm[particle][2]-target) < abs(function(*swarm[particle][4])-target):
                     swarm[particle][4] = swarm[particle][3].copy()
 
-                    # Find if current position is closer to target than global best
+                    # Find if position is closer to target than global best
                     if abs(swarm[particle][2]-target) < abs(best-target):
                         coordinates = swarm[particle][3].copy()
                         best = swarm[particle][2]
 
-            # Calculate the eucleidian distance between the particle's position and the global best position
-            distance_from_global_best += ndistance(swarm[particle][3], coordinates)
+            # Calculate the eucleidian distance between
+            # the particle's position and global best
+            distance_from_global_best += ndistance(swarm[particle][3],
+                                                   coordinates)
 
         # Convergence check, and break loop if satisfied
         if abs(best - distance_from_global_best) < convergence:
